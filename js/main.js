2230d32c07bef615a6897636f9ee794997006e0e
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-18 17:12:50 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('.main_slide').slick({
+        arrows: false,
+        // fade: true,
+        // vertical:true,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('.main_visual .dots li').on('mouseenter', function () {
+        //내가 클릭한 요소의 번호를 알려주는 것
+        let idx = $(this).index();
+        console.log(idx);
+        $('.main_slide').slick('slickGoTo', idx, true);
+    });
+
+    //아이콘으로 이전, 다음 버튼 만들기
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev');
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext');
+    });
+});
